96fa711320fd41ce586fbe628ce4458b3f13d55a
Joel Rorseth <joelrorseth@gmail.com>
2018-03-17 16:57:24 -0400

    Start classification script, read in selected genes

--- Cancer Stage Diagnosis/stage_classification.r
+++ Cancer Stage Diagnosis/stage_classification.r
@@ -12,16 +12,29 @@
 setwd("/Users/Joel/Documents/Research/Cancer Stage Diagnosis/Datasets")
 patient_data <- read.csv("anon_patient_expressions_stage_all.csv", header=T, fill=T)
 
-# Column missing value filtering moved to preprocessing scripts
+# Read in list of feature selected genes
+selected_genes <- readLines("genes_tree_top_1000.txt")
+
+# Separate predictors and response, X and y
+patient_stages <- patient_data[,ncol(patient_data)]
+patient_data <- patient_data[,-ncol(patient_data)]
+
+new_genetic_data <- patient_data[, colnames(patient_data) %in% selected_genes]
 
 
-#patient_stages <- patient_data[,ncol(patient_data)]
-#patient_data <- patient_data[,-ncol(patient_data)]
-
-# TODO
 # SECTION 1: Try SVM
-#library(e1071)
-#svm_clf <- svm(patient_stages~., data=data.matrix(patient_data), kernel="polynomial", cost=10, scale=FALSE)
+library(e1071)
+svm_clf <- svm(as.factor(patient_stages)~., data=data.matrix(new_genetic_data), 
+               kernel="polynomial", cost=10, scale=FALSE)
+plot(svm_clf, data.matrix(new_genetic_data))
 
-#library(tree)
-#tree.patients <- tree(patient_stages~., patient_data)
+
+# SECTION 2: Random Forest
+library(tree)
+tree.patients <- tree(as.factor(patient_stages)~., new_genetic_data)
+
+plot(tree.patients)
+text(tree.patients, pretty=0)
+
+
+# TODO: KNN

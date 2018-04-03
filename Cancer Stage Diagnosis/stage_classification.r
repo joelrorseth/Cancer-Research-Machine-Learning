4a307dda272bd7913496643702d693f7768f1d9f
Joel Rorseth <joelrorseth@gmail.com>
2018-04-02 23:31:51 -0400

    Normalize multiclass problem to evaluate all pairs of stages

--- Cancer Stage Diagnosis/stage_classification.r
+++ Cancer Stage Diagnosis/stage_classification.r
@@ -12,10 +12,11 @@ library(tree)
 library(e1071)
 
 
-classify_selected <- function(X, y, selected_genes) {
+# Perform classification using feature selection
+classify_selected <- function(X, y, filename_selected, limit) {
   
-  # Get feature selected X
-  selected_genes = readLines("genes_rfe_top_all.txt")[0:100]
+  # Get feature selected X using feature selected genes provided
+  selected_genes = readLines(filename_selected)[0:limit]
   new_genetic_data <- X[, colnames(X) %in% selected_genes]
   
   # Split up data to get train / test dataset
@@ -27,11 +28,11 @@ classify_selected <- function(X, y, selected_genes) {
   
   
   # SECTION 1: KNN
-  knn_class <- knn(train.X, test.X, as.factor(train.y), k=3)
+  #knn_class <- knn(train.X, test.X, as.factor(train.y), k=3)
   
   # Print confusion matrix and test classification score
   # print( table(knn_class, test.y) )
-  print( mean(knn_class == test.y) ) # Mean classification score
+  #print( mean(knn_class == test.y) ) # Mean classification score
   
   
   # SECTION 2: Try SVM
@@ -41,11 +42,17 @@ classify_selected <- function(X, y, selected_genes) {
   
   
   # SECTION 3: Random Forest
-  #tree.patients <- tree(as.factor(patient_stages)~., new_genetic_data)
+  tree.patients <- tree(as.factor(y)~., new_genetic_data, subset=train_filter)
+  tree.pred <- predict(tree.patients, test.X, type="class")
+  
+  tab <- table(tree.pred, test.y)
+  print(mean(tree.pred==test.y))
   
   #plot(tree.patients)
   #text(tree.patients, pretty=0) 
 }
+
+
 
 
 
@@ -58,39 +65,37 @@ patient_stages <- patient_data[,ncol(patient_data)]
 patient_data <- patient_data[,-ncol(patient_data)]
 
 
+# Perform classification using top 10 selected features from file
+# ~48% accuracy
+#classify_selected(patient_data, patient_stages, "genes_rfe_top_all.txt", 10)
 
-# Section: RFE
-# Try KNN on the top n genes
-for (n in c(5, 10, 50, 100, 200, 500, 1000)) {
-  
-  # Get feature selected X
-  selected_genes = readLines("genes_rfe_top_all.txt")[0:n]
-  new_genetic_data <- patient_data[, colnames(patient_data) %in% selected_genes]
-  
-  # Split up data to get train / test dataset
-  train_filter = sort(sample(nrow(new_genetic_data), nrow(new_genetic_data)*.7))
-  train.X <- new_genetic_data[train_filter,]
-  test.X <- new_genetic_data[-train_filter,]
-  train.y <- patient_stages[train_filter]
-  test.y <- patient_stages[-train_filter]
-  
-  
-  # Try KNN
-  knn_class <- knn(train.X, test.X, as.factor(train.y), k=3)
-  
-  # Print confusion matrix and test classification score
-  print( mean(knn_class == test.y) )
-}
+# ~47% accuracy
+#classify_selected(patient_data, patient_stages, "genes_tree_top_50.txt", 10)
 
 
 
+# This data has a class imbalance problem
+# We will test classification between all possible pairs of the 5 classes
 
-for (selection in c("genes_tree_top_50.txt", "genes_tree_top_100.txt", 
-                    "genes_tree_top_500.txt", "genes_tree_top_1000.txt")) {
-  
-  # Read in list of feature selected genes
-  selected_genes <- readLines(selection)
-  
-  # Run classifiers on this feature subset
-  classify_selected(patient_data, patient_stages, selected_genes)
+
+# Generate each unique pair of classes / stages (0...4)
+for (i in c(0,1,2,3)) {
+  for (j in (i+1):4) {
+    
+    # Determine rows which are stage i or j
+    i_indices <- which(patient_stages==i)
+    j_indices <- which(patient_stages==j)
+    ij_indices <- which(patient_stages==i | patient_stages==j)
+    
+    # Form new matrix by reducing to i or j rows only
+    patient_data_ij = patient_data[ij_indices,]
+    patient_stages_ij <- patient_stages[ij_indices]
+    
+    # Classify this instead
+    s <- sprintf("Classifying Stage %d vs. %d, each having %d and %d samples", 
+                 i, j, length(i_indices), length(j_indices))
+    print(s)
+    classify_selected(patient_data_ij, patient_stages_ij, 
+                      "genes_rfe_top_all.txt", 20)
+  }
 }

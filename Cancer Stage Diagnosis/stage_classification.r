dc8dd5b990b4db8dc32a3cf19c42272f2dd39a4d
Joel Rorseth <joelrorseth@gmail.com>
2018-08-04 18:52:19 -0400

    Update documentation in stage classification script

--- Cancer Stage Diagnosis/stage_classification.r
+++ Cancer Stage Diagnosis/stage_classification.r
@@ -10,10 +10,16 @@
 library(class)
 library(tree)
 library(e1071)
+library(caret)
+library(ROCR)
+library(mccr)
 
+# This file is a work in progress
+# The lass imbalance is reflected in these experiments
+# TODO: Incorporate imbalance strategies, perhaps SMOTE
 
 # SVM classification
-classify.svm <- function(X, y, train_indices, filename, top_n) {
+classify.svm <- function(X, y, train_indices, filename, top_n, i, j) {
   
   # Get feature selected gene names
   selected_genes = readLines(filename)[0:top_n]
@@ -32,15 +38,42 @@ classify.svm <- function(X, y, train_indices, filename, top_n) {
                    ranges=list(cost=c(0.1,1,10,100,1000), gamma=c(0.5,1,2,3,4)))
   
   # And use to predict
-  #table(true=dat[-train_indices,"y"], 
-  #      pred=predict(tune.out$best.model, newdata=dat[-train_indices,]))
+  table(true=dat[-train_indices,"y"], 
+        pred=predict(tune.out$best.model, newdata=dat[-train_indices,]))
   
   svm.pred <- predict(tune.out$best.model, newdata=dat[-train_indices,])
   svm.mean <- mean(svm.pred==dat[-train_indices,"y"])
   
+  #print(typeof (list(svm.pred)))
+  #print(dat[-train_indices, "y"])
+  #prediction <- prediction(list(svm.pred), dat[-train_indices, "y"])
+  #svm.mcc <- performance(prediction, "mat")
+  
   print(sprintf( "SVM CLF: %f", svm.mean ))
-}
+  #print(svm.mcc)
 
+  p_ <- svm.pred
+  y_ <- dat[-train_indices, "y"]
+  p_[p_==i] <- 0
+  p_[p_==j] <- 1
+  y_[y_==i] <- 0
+  y_[y_==j] <- 1
+  
+  #print(y_)
+  #print(p_)
+  #m <- mccr(y_, p_)
+  #print(m)
+  
+  
+  #tp = sum((p_==j)*(y_==j))/sum(y_==j)
+  #print(tp)
+  #fp = sum((p_==j)*(y_==i))/sum(y_==i)
+  #print(fp)
+  #print(sprintf( "TP: %f \tFP: %f", tp, fp ))
+  
+  c <- confusionMatrix(data=svm.pred, reference=dat[-train_indices, "y"], mode = "prec_recall")
+  print(c)
+}
 
 
 # KNN classifier -- this is more or less a baseline classification
@@ -62,7 +95,6 @@ classif.knn <- function(train.X, train.y, test.X, test.y, filename, top_n) {
   
   #tab <- table(knn.clf, test.y)
 }
-
 
 
 # Tree classification -- this should be one of the most promising
@@ -90,6 +122,48 @@ classif.tree <- function(X, y, train_indices, filename, top_n) {
 }
 
 
+# Run several classifiers between all possible pairs of classes
+one_vs_one_classify <- function(X, y) {
+  
+  # Generate each unique pair of classes / stages (0...4)
+  for (i in c(0,1,2,3)) {
+    for (j in (i+1):4) {
+      
+      # Determine rows which are stage i or j
+      i_indices <- which(y==i)
+      j_indices <- which(y==j)
+      ij_indices <- which(y==i | y==j)
+      
+      # Form new matrix by reducing to i or j rows only
+      ij.X = X[ij_indices,]
+      ij.y <- y[ij_indices]
+      
+      # Split i, j data into train and test sets
+      train_indices <- createDataPartition(as.factor(ij.y), p = .7, list = F, times = 1)
+      train.X <- ij.X[train_indices,]
+      test.X <- ij.X[-train_indices,]
+      train.y <- ij.y[train_indices]
+      test.y <- ij.y[-train_indices]
+      
+      
+      s <- sprintf("Classifying Stage %d vs. %d, each having %d and %d samples", 
+                   i, j, length(i_indices), length(j_indices))
+      print(s)
+      print(sprintf( "Train size = %d, Test size = %d", length(train.y), length(test.y) ))
+      
+      
+      if (length(train.y) == 0) next
+      if (length(test.y) == 0) next
+      
+      # Perform classification for this 1v1 classification using different models
+      classif.tree(ij.X, ij.y, train_indices, "genes_mRMR_top_50.txt", 7)
+      classif.knn(train.X, train.y, test.X, test.y, "genes_mRMR_top_50.txt", 7)
+      classify.svm(ij.X, ij.y, train_indices, "genes_mRMR_top_50.txt", 10, i, j)
+      print(" ")
+    }
+  }
+}
+
 
 
 # Read preprocessed patient data
@@ -98,48 +172,9 @@ patient_data <- read.csv("anon_patient_expressions_stage_all.csv", header=T, fil
 
 # Separate predictors and response, X and y
 X <- patient_data[,-ncol(patient_data)]
-y <- patient_data[,ncol(patient_data)]
+y <- factor(patient_data[,ncol(patient_data)])
 
-
-
-# This data has a class imbalance problem
 # Straight classification w/ feature selection yielded ~47% avg accuracy
-# We will now try classification between all possible pairs of the 5 classes
+# Instead manually attempt one vs one strategy to handle multiclass problem
 
-# Generate each unique pair of classes / stages (0...4)
-for (i in c(0,1,2,3)) {
-  for (j in (i+1):4) {
-    
-    # Determine rows which are stage i or j
-    i_indices <- which(y==i)
-    j_indices <- which(y==j)
-    ij_indices <- which(y==i | y==j)
-    
-    # Form new matrix by reducing to i or j rows only
-    ij.X = X[ij_indices,]
-    ij.y <- y[ij_indices]
-    
-    # Split i, j data into train (80%) and test (20%) sets
-    train_indices = sort(sample(nrow(ij.X), nrow(ij.X)*.8))
-    train.X <- ij.X[train_indices,]
-    test.X <- ij.X[-train_indices,]
-    train.y <- ij.y[train_indices]
-    test.y <- ij.y[-train_indices]
-    
-
-    s <- sprintf("Classifying Stage %d vs. %d, each having %d and %d samples", 
-                 i, j, length(i_indices), length(j_indices))
-    print(s)
-    print(sprintf( "Train size = %d, Test size = %d", length(train.y), length(test.y) ))
-    
-    
-    if (length(train.y) == 0) next
-    if (length(test.y) == 0) next
-    
-    # Perform classification for this 1v1 classification using different models
-    classif.tree(ij.X, ij.y, train_indices, "genes_mRMR_top_50.txt", 7)
-    classif.knn(train.X, train.y, test.X, test.y, "genes_mRMR_top_50.txt", 7)
-    classify.svm(ij.X, ij.y, train_indices, "genes_mRMR_top_50.txt", 10)
-    print(" ")
-  }
-}
+one_vs_one_classify(X, y)
